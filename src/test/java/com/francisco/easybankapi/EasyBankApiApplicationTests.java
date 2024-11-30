108795d0d4abd99d34ee55ca66cb5742ae221e8b
Francisco Sousa <franciscoeds1987@gmail.com>
2024-11-30 10:48:26 -0300

    Initial commit

---
+++ src/test/java/com/francisco/easybankapi/EasyBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.easybankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
