b7f44199714e11a7ac4262d609690b85dee8d146
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-01 10:01:35 -0300

    Creating our own custom tables for Authentication

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -21,3 +21,17 @@ INSERT IGNORE INTO `users`
 VALUES ('admin', '{bcrypt}$2a$12$IwCVDkCnVDme9z8/Tdfy.umuYkk/ywS8FIZwYP1Ksq8OHEvI5QoVW', '1');
 INSERT IGNORE INTO `authorities`
 VALUES ('admin', 'admin');
+
+CREATE TABLE `customer`
+(
+    `id`    int          NOT NULL AUTO_INCREMENT,
+    `email` varchar(45)  NOT NULL,
+    `pwd`   varchar(200) NOT NULL,
+    `role`  varchar(45)  NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+VALUES ('happy@example.com', '{noop}Francisco@12345', 'read');
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+VALUES ('admin@example.com', '{bcrypt}$2a$12$IwCVDkCnVDme9z8/Tdfy.umuYkk/ywS8FIZwYP1Ksq8OHEvI5QoVW', 'admin');
