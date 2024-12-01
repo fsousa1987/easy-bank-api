0a80d2bee793686d6295fd478aad8f32c8a5015c
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-01 11:30:47 -0300

    Building a new REST API to allow the registration of new User

--- src/main/java/com/francisco/easybankapi/config/ProjectSecurityConfig.java
+++ src/main/java/com/francisco/easybankapi/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,9 +19,11 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/my-account", "/my-balance", "/my-loans", "/my-cards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/my-account", "/my-balance", "/my-loans", "/my-cards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
