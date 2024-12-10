2ce951c6b1762b69c33ad5e3242333767f3316ee
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-10 17:25:54 -0300

    Configuring Roles Authorization inside web application using Spring Security

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -223,3 +223,11 @@ INSERT INTO authorities (customer_id, name)
 VALUES (1, 'VIEWLOANS');
 INSERT INTO authorities (customer_id, name)
 VALUES (1, 'VIEWBALANCE');
+
+DELETE
+FROM authorities;
+
+INSERT INTO authorities(customer_id, name)
+VALUES (1, 'ROLE_USER');
+INSERT INTO authorities(customer_id, name)
+VALUES (1, 'ROLE_ADMIN');
