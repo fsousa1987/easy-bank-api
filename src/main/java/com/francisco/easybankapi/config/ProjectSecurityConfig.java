a48bb910cacbec644a779a563fab90e428e16afd
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-12 11:08:41 -0300

    Making project configuration to use JWT tokens

--- src/main/java/com/francisco/easybankapi/config/ProjectSecurityConfig.java
+++ src/main/java/com/francisco/easybankapi/config/ProjectSecurityConfig.java
@@ -2,10 +2,7 @@ package com.francisco.easybankapi.config;
 
 import com.francisco.easybankapi.exceptionhandling.CustomAccessDeniedHandler;
 import com.francisco.easybankapi.exceptionhandling.CustomBasicAuthenticationEntryPoint;
-import com.francisco.easybankapi.filter.AuthoritiesLoggingAfterFilter;
-import com.francisco.easybankapi.filter.AuthoritiesLoggingAtFilter;
-import com.francisco.easybankapi.filter.CsrfCookieFilter;
-import com.francisco.easybankapi.filter.RequestValidationBeforeFilter;
+import com.francisco.easybankapi.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -22,6 +19,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,15 +30,14 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-        http
-                .securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
-                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(corsConfig -> corsConfig.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                     config.setAllowedMethods(Collections.singletonList("*"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(List.of("Authorization"));
                     config.setMaxAge(3600L);
                     return config;
                 }))
@@ -52,6 +49,8 @@ public class ProjectSecurityConfig {
                 .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
                 .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
                 .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
                 .authorizeHttpRequests((requests) -> requests
                         /* .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
