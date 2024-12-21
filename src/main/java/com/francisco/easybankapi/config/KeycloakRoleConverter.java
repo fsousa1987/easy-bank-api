340d063e9fdd563d535790fd9e12a496204394aa
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-21 08:46:26 -0300

    Client credentials grant type flow demo with Spring Auth Server

--- src/main/java/com/francisco/easybankapi/config/KeycloakRoleConverter.java
+++ src/main/java/com/francisco/easybankapi/config/KeycloakRoleConverter.java
@@ -9,8 +9,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
@@ -18,21 +16,15 @@ public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedA
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> realmAccess = objectMapper
-                .convertValue(source.getClaims().get("realm_access"), new TypeReference<>() {
-                });
 
-        if (realmAccess == null || realmAccess.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        List<String> roles = objectMapper.convertValue(realmAccess.get("roles"), new TypeReference<>() {
+        ArrayList<String> roles = objectMapper.convertValue(source.getClaims().get("scope"), new TypeReference<>() {
         });
 
-        return roles.stream()
-                .map(roleName -> "ROLE_" + roleName)
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+        if (roles == null || roles.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return roles.stream().map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
 }
